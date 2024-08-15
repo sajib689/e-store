9c977ae4833db8f3c936e03b56cf9ba1557fe540
sajib689 <jibonraja11@gmail.com>
2024-08-15 20:39:50 +0600

    react star icon add

--- src/Components/ProductsCard.jsx
+++ src/Components/ProductsCard.jsx
@@ -1,5 +1,15 @@
+import ReactStars from "react-rating-stars-component";
+
 const ProductsCard = ({ product }) => {
-  const { productImage,productName,description,price,ratings,category,creationDate } = product;
+  const {
+    productImage,
+    productName,
+    description,
+    price,
+    ratings,
+    category,
+    creationDate,
+  } = product;
   console.log(product);
   return (
     <div>
@@ -17,45 +27,22 @@ const ProductsCard = ({ product }) => {
           </h1>
 
           <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit In odit
+            {description}
           </p>
 
           <div className="flex mt-2 items-center">
-            <svg
-              className="w-5 h-5 text-gray-700 fill-current dark:text-gray-300"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
-            <svg
-              className="w-5 h-5 text-gray-700 fill-current dark:text-gray-300"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
-            <svg
-              className="w-5 h-5 text-gray-700 fill-current dark:text-gray-300"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
-            <svg
-              className="w-5 h-5 text-gray-500 fill-current"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
-            <svg
-              className="w-5 h-5 text-gray-500 fill-current"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
+            <ReactStars
+              count={5}
+              value={ratings}
+              size={24}
+              activeColor="#ffd700"
+            />
+            ,
           </div>
 
           <div className="flex justify-between mt-3 items-center">
             <h1 className="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">
-              $220
+              ${price}
             </h1>
             <button className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-[#0F42AB] rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">
               Add to Cart

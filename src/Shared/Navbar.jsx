58819fb652443ecdbd5addf1bd20d6af1de521ee
sajib689 <jibonraja11@gmail.com>
2024-08-15 11:37:14 +0600

    add home page

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,4 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+    </>
+  );
   return (
     <div>
       <div className="navbar container mx-auto">
@@ -24,48 +33,16 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <Link to="/">
+            <img className="w-24 h-24" src="./logo.png" alt="" />
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+           {links}
           </ul>
         </div>
         <div className="navbar-end">

7e560628bb9fb162046021f1a35f38878d3f2594
sajib689 <jibonraja11@gmail.com>
2024-08-16 17:56:41 +0600

    fix an issu

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,51 +1,41 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import toast, { Toaster } from "react-hot-toast";
 
 const Navbar = () => {
-  const { user,
-    createUser,
-    createUserGoogle,
-    userLogin,
-    userSignOut} = useContext(AuthContext)
+  const { user, createUser, createUserGoogle, userLogin, userSignOut } =
+    useContext(AuthContext);
   const openModal = () => {
-    document.getElementById('sign_up_modal').showModal();
+    document.getElementById("sign_up_modal").showModal();
   };
 
   const closeModal = () => {
-    document.getElementById('sign_up_modal').close();
+    document.getElementById("sign_up_modal").close();
   };
-  const handlerCreateUser = e => {
-    e.preventDefault()
-    const form = e.target
-    const name = form.name.value
-    const email = form.email.value
-    const password = form.password.value
-    const user = {name, email, password}
+  const handlerCreateUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const user = { name, email, password };
     createUser(email, password)
-    .then( result => {
-      const user = result.user
-      if(user){
-        Swal.fire({
-          position: "top-center",
-          icon: "success",
-          title: "Sign Up Success!",
-          showConfirmButton: false,
-          timer: 1500
-        });
-      }
-    })
-    .catch(err => {
-      if(err) {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: `${err.message}`,
-          
-        });
-      }
-    })
-  }
+      .then((result) => {
+        const user = result.user;
+        if (user) {
+          toast.success("Successfully Sign Up");
+        }
+        form.reset();
+        closeModal();
+      })
+      .catch((err) => {
+        if (err) {
+          toast.error(`${err.message}`);
+        }
+      });
+  };
   const links = (
     <>
       <li>
@@ -61,80 +51,120 @@ const Navbar = () => {
         <Link to="/">Contact Us</Link>
       </li>
     </>
-    
   );
   return (
-   <>
-    <div className="bg-base-200 font-serif">
-      <div className="navbar container mx-auto">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <div className="bg-base-200 font-serif">
+        <div className="navbar container mx-auto">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu font-bold menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                {links}
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu font-bold menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              {links}
-            </ul>
+            <Link to="/">
+              <img className="w-24 h-24" src="./logo.png" alt="" />
+            </Link>
           </div>
-          <Link to="/">
-            <img className="w-24 h-24" src="./logo.png" alt="" />
-          </Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu font-bold menu-horizontal px-1">{links}</ul>
-        </div>
-        <div className="navbar-end">
-          <button  onClick={openModal} className="bg-[#0F42AB] text-white btn transition-all hover:bg-[#0e4ed0]">Login</button>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu font-bold menu-horizontal px-1">{links}</ul>
+          </div>
+          <div className="navbar-end">
+            <button
+              onClick={openModal}
+              className="bg-[#0F42AB] text-white btn transition-all hover:bg-[#0e4ed0]"
+            >
+              Login
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-{/* modal area */}
-    <dialog id="sign_up_modal" className="modal">
+      {/* modal area */}
+      <dialog id="sign_up_modal" className="modal">
         <div className="modal-box">
-          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={closeModal}>✕</button>
+          <button
+            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            onClick={closeModal}
+          >
+            ✕
+          </button>
           <h3 className="font-bold text-lg">Sign Up</h3>
           <form onSubmit={handlerCreateUser} className="py-4" method="dialog">
             <div className="form-control">
               <label htmlFor="username" className="label">
                 <span className="label-text">Username</span>
               </label>
-              <input name="name" id="username" type="text" placeholder="Enter your username" className="input input-bordered" required />
+              <input
+                name="name"
+                id="username"
+                type="text"
+                placeholder="Enter your username"
+                className="input input-bordered"
+                required
+              />
             </div>
             <div className="form-control mt-4">
               <label htmlFor="email" className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input name="email" id="email" type="email" placeholder="Enter your email" className="input input-bordered" required />
+              <input
+                name="email"
+                id="email"
+                type="email"
+                placeholder="Enter your email"
+                className="input input-bordered"
+                required
+              />
             </div>
             <div className="form-control mt-4">
               <label htmlFor="password" className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input name="password" id="password" type="password" placeholder="Enter your password" className="input input-bordered" required />
+              <input
+                name="password"
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                className="input input-bordered"
+                required
+              />
             </div>
             <div className="form-control mt-4">
-              <button type="submit" className="btn text-white bg-[#0F42AB] hover:bg-[#0F42AB]">Sign Up</button>
+              <button
+                type="submit"
+                className="btn text-white bg-[#0F42AB] hover:bg-[#0F42AB]"
+              >
+                Sign Up
+              </button>
             </div>
           </form>
         </div>
       </dialog>
-   </>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import AuthProvider from "./Provider/AuthProvider";
+import { Toaster } from "react-hot-toast";
 
 // Create a QueryClient instance
 const queryClient = new QueryClient();
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")).render(
    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router}>
       <Main />
+      <Toaster/>
     </RouterProvider>
   </QueryClientProvider>
  </AuthProvider>
